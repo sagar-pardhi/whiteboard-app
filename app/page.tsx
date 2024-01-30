1542c0b8d2871217fcc8909598cc603605b10431
sagar-pardhi <sgrpardhi007@gmail.com>
2024-01-30 13:43:06 +0530

    setup clerk and convex

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <h1>Hello</h1>;
+  return (
+    <div className="flex flex-col gap-y-4">
+      <div>
+        <UserButton />
+      </div>
+    </div>
+  );
 }
