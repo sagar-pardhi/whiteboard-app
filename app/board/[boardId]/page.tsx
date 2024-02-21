f4ecdd9a9dedb9cc99734bcb1892d50e384c1d2d
sagar-pardhi <sgrpardhi007@gmail.com>
2024-02-21 14:49:17 +0530

    add liveblocks setup

--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -1,4 +1,6 @@
+import { Room } from "@/components/room";
 import { Canvas } from "./components/canvas";
+import { Loading } from "./components/loading";
 
 interface BoardIdPageProps {
   params: {
@@ -7,6 +9,10 @@ interface BoardIdPageProps {
 }
 
 const BoardIdPage = ({ params }: BoardIdPageProps) => {
-  return <Canvas boardId={params.boardId} />;
+  return (
+    <Room roomId={params.boardId} fallback={<Loading />}>
+      <Canvas boardId={params.boardId} />;
+    </Room>
+  );
 };
 export default BoardIdPage;
