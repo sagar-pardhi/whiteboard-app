46e006ff96143ffe7a572d89e46938b688c757b0
sagar-pardhi <sgrpardhi007@gmail.com>
2024-02-22 13:58:04 +0530

    add room toolbar

---
+++ app/board/[boardId]/_components/tool-button.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Hint } from "@/components/hint";
+import { Button } from "@/components/ui/button";
+import { LucideIcon } from "lucide-react";
+
+interface ToolButtonProps {
+  label: string;
+  icon: LucideIcon;
+  isActive?: boolean;
+  isDisabled?: boolean;
+  onClick: () => void;
+}
+
+export const ToolButton = ({
+  icon: Icon,
+  label,
+  isActive,
+  isDisabled,
+  onClick,
+}: ToolButtonProps) => {
+  return (
+    <Hint label={label} side="right" sideOffset={14}>
+      <Button
+        disabled={isDisabled}
+        onClick={onClick}
+        size="icon"
+        variant={isActive ? "boardActive" : "board"}
+      >
+        <Icon />
+      </Button>
+    </Hint>
+  );
+};
