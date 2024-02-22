e19f6c40e53528bcfe3032b4603d34094e93c4a8
sagar-pardhi <sgrpardhi007@gmail.com>
2024-02-22 13:59:51 +0530

    updated hint component

--- components/hint.tsx
+++ components/hint.tsx
@@ -8,8 +8,8 @@ import {
 export interface HintProps {
   label: string;
   children: React.ReactNode;
-  side: "top" | "bottom" | "left" | "right";
-  align: "start" | "center" | "end";
+  side?: "top" | "bottom" | "left" | "right";
+  align?: "start" | "center" | "end";
   sideOffset?: number;
   alignOffset?: number;
 }
