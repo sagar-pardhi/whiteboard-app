8689052050f03ea8cc1f3f313115a38520c01e9d
sagar-pardhi <sgrpardhi007@gmail.com>
2024-02-23 14:22:56 +0530

    add insert layer functionality

---
+++ app/board/[boardId]/_components/layer-preview.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useStorage } from "@/liveblocks.config";
+import { LayerType } from "@/types/canvas";
+import { memo } from "react";
+import { Rectangle } from "./rectangle";
+
+interface LayerPreviewProps {
+  id: string;
+  selectionColor?: string;
+  onLayerPointDown: (e: React.PointerEvent, layerId: string) => {};
+}
+
+export const LayerPreview = memo(
+  ({ id, selectionColor, onLayerPointDown }: LayerPreviewProps) => {
+    const layer = useStorage((root) => root.layers.get(id));
+
+    if (!layer) return;
+
+    switch (layer.type) {
+      case LayerType.Rectangle:
+        return (
+          <Rectangle
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointDown}
+            selectionColor={selectionColor}
+          />
+        );
+      default:
+        console.warn("Unknown layer type");
+        return null;
+    }
+  }
+);
+
+LayerPreview.displayName = "LayerPreview";
